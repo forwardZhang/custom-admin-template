9fbb58617a733ec56a6fcaa6ed0032778c88b2be
zhangjinlei <zhangjl>
2025-09-23 11:27:20 +0800

    接入eslint

--- apps/adp/src/App.vue
+++ apps/adp/src/App.vue
@@ -2,15 +2,18 @@
   <div>1</div>
 </template>
 <script setup lang="ts">
-  console.log('1');
+console.log("1");
 
-  const abc = 1;
-  console.log('abc', abc);
-  console.log(abc);
+const abc = 1;
+console.log("abc", abc);
+console.log(abc);
 </script>
 
 <style scoped lang="scss">
-  div {
-    color: red;
-  }
+div {
+  color: red;
+  height: 100px;
+
+  width: 100px;
+}
 </style>
