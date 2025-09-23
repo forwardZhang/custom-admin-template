d29055b7e864c041d3d83132f1532555ef63a4d7
zhangjinlei <zhangjl>
2025-09-23 12:17:52 +0800

    设置eslint

--- apps/adp/src/App.vue
+++ apps/adp/src/App.vue
@@ -2,18 +2,17 @@
   <div>1</div>
 </template>
 <script setup lang="ts">
-console.log("1");
+  console.log('1');
 
-const abc = 1;
-console.log("abc", abc);
-console.log(abc);
+  const abc = 1;
+  console.log('abc', abc);
+  console.log(abc);
 </script>
 
 <style scoped lang="scss">
-div {
-  color: red;
-  height: 100px;
-
-  width: 100px;
-}
+  div {
+    color: red;
+    height: 100px;
+    width: 100px;
+  }
 </style>
